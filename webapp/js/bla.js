57df483ffcd7b666fcd82f9de3f7e3665fbd0b11
Gottschlich <timon.gottschlich@gmx.de>
2015-11-28 14:41:11 +0100

    upload, datenmodell
    einige veränderungen zum uploadprozess und dem datenmodell

--- webapp/js/bla.js
+++ webapp/js/bla.js
@@ -11,7 +11,7 @@
 var consoleAppender = JL.createConsoleAppender('consoleAppender');
 JL().setOptions({"appenders": [consoleAppender]});
 
-function ReadPolyline(event) {
+function Readdata(event) {
 	
 	// Init
 	var input = event.target;
@@ -27,6 +27,56 @@ function ReadPolyline(event) {
         	
 		// Read the file
 		reader.readAsText(input.files[0]);
+		//...
 };
 };
+
+
+function download() {
+    window.open('http://' + window.location.host + '/download?fileDownload=true');
+}
+
+
+$.ajax({
+    type: 'POST',
+    dataType: 'tex',
+    url: 'http://' + window.location.host + '/download',
+    timeout: 5000,
+    success: function(content, textStatus ){
+       //...
+     console.log('publication downloaded!');
+        
+
+    },
+    error: function(xhr, textStatus, errorThrown){
+        console.error('publicationdownload error!');
+    }
+});
+
+
+function newPub() {
+    bootbox.dialog({
+        title: "Please enter details for your publication!",
+        message: '<div class="input-group"><span class="input-group-addon" id="sizing-addon2">Publicationname</span><input id="pubname" type="text" class="form-control" placeholder="..." aria-describedby="sizing-addon2"></div>'
+                    + '<br> <div class="input-group"><span class="input-group-addon" id="sizing-addon2">Authorname(s)</span><input id="authorname" type="text" class="form-control daypicker" placeholder="..." aria-describedby="sizing-addon2"></div>'
+                    + '<br> <div class="input-group"><span class="input-group-addon" id="sizing-addon2">Releasedate of the publication</span><input id="releasedate" type="text" class="form-control daypicker" placeholder="Form: YYYY-MM-DD" aria-describedby="sizing-addon2"></div>',
+                    //+ '<br> <div class="input-group"><span class="input-group-addon" id="sizing-addon2">Upload</span><input type='file' accept='tex' class="btn btn-default" onchange='Readdata(event)' aria-describedby="sizing-addon2"></div>',
+
+	    		
+        onEscape: function() {},
+        buttons: {
+            "Let's Go": {         
+                callback: function(){
+                    //...
+                     //$("#publicationlist").append('<tr><td><button data-index="' + (index+1) + '" onclick="loadpub(this)" class="btn btn-default">''</button> <br></td></tr>');
+                }
+            }
+        }
+    });
+}
+
+function loadpub(button) {
+    window.location.hash = '#' + ($(button).data('index') - 1);
+}
+
 
